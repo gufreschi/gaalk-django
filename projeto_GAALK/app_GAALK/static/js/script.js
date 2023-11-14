282443f55a98b68e6eec813e084b264356afe3ef
gufreschi <gufreschi.ads@gmail.com>
2023-11-14 18:24:35 -0300

    mensagens adicionadas

--- projeto_GAALK/app_GAALK/static/js/script.js
+++ projeto_GAALK/app_GAALK/static/js/script.js
@@ -12,3 +12,8 @@ sidebar.classList.toggle("close");
 searchBtn.addEventListener("click" , () =>{
 sidebar.classList.remove("close");
 })
+
+function closeMessage() {
+    var messageContainer = document.getElementById('message-container');
+    messageContainer.style.display = 'none';
+}
