4ab1c4dd70f67d3f9d4d5cb575cc1db0c258b42d
gufreschi <gufreschi.ads@gmail.com>
2023-11-14 14:51:22 -0300

    FUNCIONALIDADES: tela home, cadastro e login com autenticacao. mensagens pop-up de ajuda aparecendo apenas no login do django

--- projeto_GAALK/app_GAALK/views.py
+++ projeto_GAALK/app_GAALK/views.py
@@ -1,20 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 def home(request):
-    return render(request, 'home.html')
-
-def cadastro(request):
-    return render(request, 'cadastro.html')
+    return render(request, 'home.html', {})
 
 def login_user(request):
     if request.method == 'POST':
-        nome_usuario = request.POST['nome_usuario']
-        senha = request.POST['senha']
-        usuario = authenticate(request, username=nome_usuario, password=senha)
-        if usuario is not None:
-            login(request, usuario)
+        username = request.POST['username']
+        senha1 = request.POST['senha1']
+        user = authenticate(request, username=username, password=senha1)
+        if user is not None:
+            login(request, user)
             messages.success(request, ('Você está conectado!'))
             return redirect('home')
         else:
@@ -28,3 +26,28 @@ def logout_user(request):
     logout(request)
     messages.success(request, ('Você foi desconectado...'))
     return redirect('login')
+
+def register_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        # data_nascimento = request.POST.get('data_nascimento')
+        # telefone = request.POST.get('telefone')
+        senha1 = request.POST.get('senha1')
+        senha2 = request.POST.get('senha2')
+        
+        user = User.objects.filter(username=username).first()
+        if user:
+            messages.error(request, ('Esse nome de usuário já existe'))
+            return redirect('cadastro')
+        
+        if senha1 == senha2:
+            user = User.objects.create_user(username=username, email=email, password=senha1)
+            user.save()
+            messages.success(request, ('Cadastro realizado com sucesso!'))
+            return redirect('login')
+        else:
+            messages.error(request, ('As senhas devem ser iguais.'))
+            return redirect('cadastro')
+    else:
+        return render(request, 'cadastro.html')
